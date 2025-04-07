058c563a125b18db44674c0cf8747b0c5451eb22
Aishik C. <136048425+Achak0120@users.noreply.github.com>
2025-04-06 21:27:57 -0500

    Main Window JFrame

--- MainProject/gui/MainWindow.java
+++ MainProject/gui/MainWindow.java
@@ -1,15 +1,50 @@
 package MainProject.gui;
 import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class MainWindow extends JFrame 
+{
+    public MainWindow() 
+    {
+        super("Window Object TESTER");
 
-public class MainWindow extends JFrame{
-    public MainWindow() {
-        super("Electrical Diagnostic");
-
-        setDefaultCloseOperation(JFRAME.EXIT_ON_CLOSE);
-
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
-
         setSize(400, 400);
+
+        // Layout
+        JPanel panel = new JPanel();
+        JPanel label = new JLabel("Im on electrial PIT CREW!!");
+        JButton button = new JButton("Button_First_Test")
+    
+        // Button Function
+        button.addActionListener(new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                label.setText("Main Window");
+            }
+        });
+    
+        // Components
+        panel.add(label);
+        panel.add(button);
+        this.add(panel);
+    }
+
+
+    public static void main(String[] args) 
+    {
+        SwingUtilities.invokeLater(() -> {
+            new MainWindow().setVisible(true);
+        })
+
     }
 }
