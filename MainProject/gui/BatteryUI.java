62257865e6b3ea08accce6da81f7ed57227f2e1e
Aishik C. <136048425+Achak0120@users.noreply.github.com>
2025-04-06 21:07:17 -0500

    a

--- MainProject/gui/BatteryUI.java
+++ MainProject/gui/BatteryUI.java
@@ -1,4 +1,7 @@
 package MainProject.gui;
+import org.knowm.xchart.*;
+import org.knowm.xchart.XChartPanel;
+import javax.swing.*;
 
 public class BatteryUI {
     
--- MainProject/gui/MainWindow.java
+++ MainProject/gui/MainWindow.java
@@ -4,13 +4,12 @@ import javax.swing.*;
 
 public class MainWindow extends JFrame{
     public MainWindow() {
-        super("MainWindow");
+        super("Electrical Diagnostic");
 
         setDefaultCloseOperation(JFRAME.EXIT_ON_CLOSE);
 
         setResizable(true);
 
         setSize(400, 400);
-        
     }
 }
