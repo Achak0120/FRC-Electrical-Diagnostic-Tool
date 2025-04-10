d45ed17fb3ab240c88167a8a04afd8152b584e12
achak <136048425+Achak0120@users.noreply.github.com>
2025-04-10 18:19:24 -0500

    MainWindow Borders

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ import javax.swing.SwingUtilities;
 import MainProject.gui.MainWindow;  // Correct import statement
 
 public class Main {
-    
+    public static void main(String[] args) {
+        MainWindow gui = new MainWindow();
+
+    }
 }
--- MainProject/gui/MainWindow.java
+++ MainProject/gui/MainWindow.java
@@ -17,15 +17,24 @@ public class MainWindow extends JFrame {
 
         // Main Panel
         JPanel main = new JPanel(new BorderLayout());
+
+        // North Left Panel 1
         JPanel NORTH_LEFT_panel1 = new JPanel();
+        JLabel panel1 = new JLabel("Panel 1");
+        NORTH_LEFT_panel1.add(panel1);
+
+        // North Left Panel 2
         JPanel NORTH_RIGHT_panel2 = new JPanel();
+        JLabel panel2 = new JLabel("Panel 2");
+        NORTH_RIGHT_panel2.add(panel2);
 
-
-
+        // MAIN NORTH PANEL
+        JPanel north = new JPanel();
+        north.add(NORTH_LEFT_panel1);
+        north.add(NORTH_RIGHT_panel2);
 
         // Add components to GUI
-        main.add(NORTH_LEFT_panel1, BorderLayout.NORTH);
-        main.add(NORTH_RIGHT_panel2, BorderLayout.NORTH);
+        main.add(north, BorderLayout.NORTH);
     }
 
     public static void main(String[] args) {
