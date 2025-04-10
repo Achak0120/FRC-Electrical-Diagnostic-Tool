5d2ce6c264b508d511757fe7241ef846311bcf73
achak <136048425+Achak0120@users.noreply.github.com>
2025-04-10 18:58:33 -0500

    a

--- MainProject/gui/MainWindow.java
+++ MainProject/gui/MainWindow.java
@@ -1,12 +1,11 @@
 package MainProject.gui;
-
-import java.awt.*;
-import awt.event.*;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import javax.swing.SwingUtilities;
 
 public class MainWindow extends JFrame {
     public MainWindow() {
@@ -15,16 +14,21 @@ public class MainWindow extends JFrame {
         setResizable(true);
         setSize(700, 700);
 
+        // Variables
+        Dimension dimension = new Dimension(300, 300);
+
         // Main Panel
         JPanel main = new JPanel(new BorderLayout());
 
         // North Left Panel 1
         JPanel NORTH_LEFT_panel1 = new JPanel();
+        NORTH_LEFT_panel1.setPreferredSize(dimension);
         JLabel panel1 = new JLabel("Panel 1");
         NORTH_LEFT_panel1.add(panel1);
 
         // North Left Panel 2
         JPanel NORTH_RIGHT_panel2 = new JPanel();
+        NORTH_RIGHT_panel2.setPreferredSize(dimension);
         JLabel panel2 = new JLabel("Panel 2");
         NORTH_RIGHT_panel2.add(panel2);
 
@@ -35,6 +39,7 @@ public class MainWindow extends JFrame {
 
         // Add components to GUI
         main.add(north, BorderLayout.NORTH);
+        this.add(main);
     }
 
     public static void main(String[] args) {
