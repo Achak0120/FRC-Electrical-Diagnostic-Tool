c1ae47e33ce0c97c1db7229b92508b0147e2a2db
achak <136048425+Achak0120@users.noreply.github.com>
2025-04-10 18:07:02 -0500

    MainWindow Tester Main

--- Main.java
+++ Main.java
@@ -2,9 +2,5 @@ import javax.swing.SwingUtilities;
 import MainProject.gui.MainWindow;  // Correct import statement
 
 public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new MainWindow().setVisible(true);
-        });
-    }
+    
 }
--- MainProject/gui/MainWindow.java
+++ MainProject/gui/MainWindow.java
@@ -1,24 +1,35 @@
 package MainProject.gui;
 
+import java.awt.*;
+import awt.event.*;
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MainWindow extends JFrame {
     public MainWindow() {
-        super("Window Object");
-
+        super("FIRST Diagnostics");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
         setSize(700, 700);
 
-        // Layout
-        JPanel panel = new JPanel();
-        JLabel label = new JLabel("I'm on electrical PIT CREW!!");
-        // Button Function
+        // Main Panel
+        JPanel main = new JPanel(new BorderLayout());
+        JPanel NORTH_LEFT_panel1 = new JPanel();
+        JPanel NORTH_RIGHT_panel2 = new JPanel();
 
-        // Components
-        panel.add(label);
-        this.add(panel);
+
+
+
+        // Add components to GUI
+        main.add(NORTH_LEFT_panel1, BorderLayout.NORTH);
+        main.add(NORTH_RIGHT_panel2, BorderLayout.NORTH);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new MainWindow().setVisible(true);
+        });
     }
 }
